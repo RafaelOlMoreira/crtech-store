5cb17108b6c90d210ac504aa8b980114b5a1a8c2
Rafael Oliveira Moreira <138674189+RafaelOlMoreira@users.noreply.github.com>
2025-02-28 23:26:30 -0300

    feat: add Navbar to the project

---
+++ src/pages/Login/LoginPage.jsx
@@ -0,0 +1,16 @@
+import NavBar from "./components/NavBar"
+
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>
+      <NavBar />
+      <div className="flex items-center justify-center h-screen bg-gray-900">
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
+
