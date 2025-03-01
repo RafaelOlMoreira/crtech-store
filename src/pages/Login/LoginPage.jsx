bbdbfd707f07dab8abe5d5b93257053ce04107de
Rafael Oliveira Moreira <138674189+RafaelOlMoreira@users.noreply.github.com>
2025-02-28 23:38:58 -0300

    feat: add Carousel to the project

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,12 +1,14 @@
 import NavBar from "./components/NavBar"
+import Carousel from "./components/Carousel"
 
 import React from 'react'
 
 function LoginPage() {
   return (
     <div>
-      <NavBar />
-      <div className="flex items-center justify-center h-screen bg-gray-900">
+        <NavBar />
+      <div className="flex items-center justify-center h-screen bg-gray-900 pt-12">
+        <Carousel />
       </div>
     </div>
   )
---
+++ src/pages/Login/components/Carousel.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import banner1 from "../../../assets/banner1.png";
+import banner2 from "../../../assets/banner2.png";
+import banner3 from "../../../assets/banner3.png";
+
+const images = [banner1, banner2, banner3];
+
+const Carousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3500);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="top-0 left-0 w-screen h-full sm:h-full md:h-full overflow-hidden z-2">
+      <div
+        className="flex transition-transform duration-700 ease-in-out"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {images.map((src, index) => (
+          <img
+            key={index}
+            src={src}
+            className="w-screen h-full object-cover flex-shrink-0"
+            alt={`Slide ${index + 1}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
