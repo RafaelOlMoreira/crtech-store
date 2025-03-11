bc49ccbd608895e381b89e1f60373beab2cc4b60
Rafael Oliveira Moreira <138674189+RafaelOlMoreira@users.noreply.github.com>
2025-03-11 17:33:56 -0300

    feat: add service section

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar"
 import Carousel from "./components/Carousel"
 import AboutSection from "./components/AboutSection"
 import ProductsSection from "./components/ProductsSection"
+import ServiceSection from "./components/ServiceSection"
 
 import React from 'react'
 
@@ -16,6 +17,8 @@ function LoginPage() {
       <AboutSection />
 
       <ProductsSection />
+
+      <ServiceSection />
 
     </div>
   )
--- src/pages/Login/components/ServiceSection.jsx
+++ src/pages/Login/components/ServiceSection.jsx
@@ -1,24 +1,15 @@
 import React from 'react'
 
-import card1 from '../../../assets/card1.png';
-import card2 from '../../../assets/card2.png';
-import card3 from '../../../assets/card3.png';
+import card1 from '../../../assets/serviceCard1.png';
+import card2 from '../../../assets/serviceCard2.png';
+import card3 from '../../../assets/serviceCard3.png';
 
-const ProductsSection = () => {
+function ServiceSection() {
     return (
-        <div className='bg-gradient-to-r from-[#1e2939] to-[#111827] min-h-screen'>
+        <div className='bg-gradient-to-l from-[#101828] to-[#0a0f1a]'>
             <section className="w-full max-w-7xl mx-auto py-16 px-4">
-                <h2 className="text-6xl font-[Poppins] text-center text-white mb-12">Produtos</h2>
+                <h2 className="text-6xl font-[Poppins] text-center text-white mb-12">Serviços</h2>
                 <div className="grid grid-cols-4 gap-4 text-white">
-                    <div className="h-117 w-80 text-center text-4xl font-[Poppins] grid place-items-center -ml-9">
-                        <div>
-                            Conheça nossos produtos da
-                            <p className="text-5xl font-bold text-[#ffb800] leading-tight">CR Tech</p>
-                            <a href="#" className="text-2xl text-black font-[Poppins] bg-[#ffb800] hover:bg-[#e0a100] p-2 rounded-lg shadow-lg transition-transform transform hover:scale-105">
-                                Saiba Mais
-                            </a>
-                        </div>
-                    </div>
                     <div className="h-117 rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105 hover:shadow-xl">
                         <img src={card1} alt="Card 1" className="w-full h-full object-cover" />
                     </div>
@@ -28,11 +19,19 @@ const ProductsSection = () => {
                     <div className="h-117 rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105 hover:shadow-xl">
                         <img src={card3} alt="Card 3" className="w-full h-full object-cover" />
                     </div>
+                    <div className="h-117 w-80 text-center text-4xl font-[Poppins] grid place-items-center ml-9">
+                        <div>
+                            Conheça nossos serviços da
+                            <p className="text-5xl font-bold text-[#ffb800] leading-tight">CR Tech</p>
+                            <a href="#" className="text-2xl text-black font-[Poppins] bg-[#ffb800] hover:bg-[#e0a100] p-2 rounded-lg shadow-lg transition-transform transform hover:scale-105">
+                                Saiba Mais
+                            </a>
+                        </div>
+                    </div>
                 </div>
-
             </section>
         </div>
     )
 }
 
-export default ProductsSection
+export default ServiceSection
