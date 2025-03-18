3ce15e0fb26eeba78faeb067715abc42751da07b
Rafael Oliveira Moreira <138674189+RafaelOlMoreira@users.noreply.github.com>
2025-03-18 18:15:07 -0300

    fix: adjust height

--- src/pages/Login/components/Carousel.jsx
+++ src/pages/Login/components/Carousel.jsx
@@ -17,8 +17,8 @@ const Carousel = () => {
   }, []);
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-900 pt-12">
-      <div className="top-0 left-0 w-screen h-full sm:h-full md:h-full overflow-hidden z-2">
+    <div className="flex items-center justify-center h-full bg-gray-900 pt-12">
+      <div className="top-0 left-0 w-screen h-full overflow-hidden z-2">
         <div
           className="flex transition-transform duration-700 ease-in-out"
           style={{ transform: `translateX(-${currentIndex * 100}%)` }}
