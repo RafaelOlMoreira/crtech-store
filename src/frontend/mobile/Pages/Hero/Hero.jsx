f059675f00bc6e22a4a7c0f05317ee50573514e1
Rafael Oliveira Moreira <138674189+RafaelOlMoreira@users.noreply.github.com>
2025-12-30 04:06:32 -0300

    reajust(Mobile): Adjusted page components.

--- src/frontend/mobile/Pages/Hero/Hero.jsx
+++ src/frontend/mobile/Pages/Hero/Hero.jsx
@@ -3,10 +3,10 @@ import HeroImage from '/HeroImage.png'
 
 function Hero() {
     return (
-        <div className='overflow-x-hidden pt-32 h-screen bg-center bg-cover bg-no-repeat flex items-center' style={{ backgroundImage: `url(${HeroImage})` }}>
-            <div className='p-5 pr-20 space-y-5'>
+        <div className='overflow-x-hidden pt-32 h-auto bg-center bg-cover bg-no-repeat flex items-center' style={{ backgroundImage: `url(${HeroImage})` }}>
+            <div className='p-5 py-10 pr-20 space-y-5'>
                 <h6 className='text-white text-[18px] bg-[#00b7ff7e] w-min text-nowrap py-0.5 px-2 rounded-lg uppercase'>Frete grátis aos fim de semana!</h6>
-                <h3 className='text-white text-[30px] font-bebasneue font-extrabold drop-shadow-xs uppercase'>Capinhas, Películas, Fones sem fio, Carregadores Rápidos</h3>
+                <h3 className='text-white text-[30px] font-bebasneue font-extrabold drop-shadow-xs uppercase'><p>Capinhas,</p> <p>Películas,</p> <p>Fones sem fio,</p> <p>Carregadores Rápidos</p></h3>
                 <p className='text-white text-[20px] font- drop-shadow-xs'>Qualidade e entrega rápida para o seu dia a dia.</p>
 
                 <div className=' space-y-2'>
