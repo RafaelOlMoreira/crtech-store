0b362d9d7308aa3745560daee5b9822c327d0fff
Rafael Oliveira Moreira <138674189+RafaelOlMoreira@users.noreply.github.com>
2026-02-03 16:08:22 -0300

    feat(Carousel/Mobile): Create Carousel.

--- src/frontend/mobile/Pages/Carrousel/Carrousel.jsx
+++ src/frontend/mobile/Pages/Carrousel/Carrousel.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+
+import Imagem1 from '/Foto1.jpg'
+import Imagem2 from '/Foto2.jpg'
+import Imagem3 from '/Foto3.jpg'
 
 function Carrousel() {
     return (
-        <section className='flex items-center justify-center h-screen bg-gray-300'>
-            <h1 className=''>Imagem</h1>
-        </section>
+        <>
+            <Carousel className=''>
+                <Carousel.Item interval={5000}>
+                    <img 
+                    className='block w-full h-screen object-cover'
+                    src={Imagem1}
+                    alt="Primeiro Slide" />
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img 
+                    className='block w-full h-screen object-cover'
+                    src={Imagem2}
+                    alt="Segundo Slide" />
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img 
+                    className='block w-full h-screen object-cover'
+                    src={Imagem3}
+                    alt="Terceiro Slide" />
+                </Carousel.Item>
+            </Carousel>
+        </>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './style.css'
 
 import Header from './frontend/mobile/Components/Header'
-import Carrousel from './frontend/mobile/Pages/Carrousel/Carrousel'
+import Carrousel from './frontend/mobile/Pages/Carrousel/Carrousel';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
     <Header />
-
     <Carrousel />
 
   </StrictMode>,
