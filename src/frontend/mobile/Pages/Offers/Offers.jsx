c28c6492d67b2b65d2a60d68bf54ce84a5533a79
Rafael Oliveira Moreira <138674189+RafaelOlMoreira@users.noreply.github.com>
2026-02-09 14:27:09 -0300

    fix(Offers/Mobile): Fixed an overlapping error in the Offers section.

--- src/frontend/mobile/Pages/Offers/Offers.jsx
+++ src/frontend/mobile/Pages/Offers/Offers.jsx
@@ -15,29 +15,10 @@ function Offers() {
 
                 {/* Produto 1 */}
                 <div className='bg-white text-black my-5 rounded-2xl'>
-                    <div className='w-full text-left'>
-                        <span className='absolute bg-red-500 text-white m-2 text-sm font-bold px-2 p-0.5 rounded-2xl'>-20%</span>
-                        <img src={Capa} alt="" className='rounded-t-2xl h-auto bg-cover bg-center bg-no-repeat' />
-                    </div>
-
-                    <div className="p-4 text-start space-y-3">
-                        <div className='space-y-1'>
-                            <span className='text-[#00B5FF] block'>Capas</span>
-                            <span className='font-semibold'>Capa 13 Pro Max</span>
-                        </div>
-                        <div className='space-x-2.5'>
-                            <span className='text-xl font-bold'>R$ 15.00</span>
-                            <span className='text-gray-700 line-through'>R$ 20.00</span>
-                        </div>
-                        <button className='bg-[#00B5FF] w-full p-1.5 text-white text-md rounded-lg'>Adicionar ao Carrinho</button>
-                    </div>
-                </div>
-                
-                {/* Produto 2 */}
-                <div className='group bg-white text-black my-5 rounded-2xl'>
-                    <div className='w-full text-left group-hover:'>
-                        <span className='absolute bg-red-500 text-white m-2 text-sm font-bold px-2 p-0.5 rounded-2xl'>-20%</span>
-                        <img src={Capa} alt="" className='rounded-t-2xl h-auto bg-cover bg-center bg-no-repeat' />
+                    <div className='rounded-t-2xl h-96 bg-cover bg-center bg-no-repeat p-2'
+                        style={{ backgroundImage: "url(/Capa.png)"}}
+                    >
+                        <span className='float-left bg-red-500 text-white text-sm font-bold px-2 p-0.5 rounded-2xl'>-20%</span>
                     </div>
 
                     <div className="p-4 text-start space-y-3">
