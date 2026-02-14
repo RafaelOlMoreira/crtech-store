9e49cf72e498c090825eaf3603b443589f82e4f4
Rafael Oliveira Moreira <138674189+RafaelOlMoreira@users.noreply.github.com>
2026-02-13 22:33:25 -0300

    refactor(Footer/Mobile): adjusted elements and texts.

--- src/frontend/mobile/Components/Footer.jsx
+++ src/frontend/mobile/Components/Footer.jsx
@@ -14,29 +14,33 @@ function Footer() {
         </span>
 
         <div className='flex space-x-4 text-white py-4'>
-          <RiInstagramFill className='size-10  bg-gray-700/60 hover:bg-[#00B5FF]/60 p-2 rounded-lg' />
-          <IoLogoWhatsapp className='size-10  bg-gray-700/60 hover:bg-[#00B5FF]/60 p-2 rounded-lg' />
+          <a href="https://www.instagram.com/cr.tech_bh">
+            <RiInstagramFill className='size-10  bg-gray-700/60 hover:bg-[#00B5FF]/60 p-2 rounded-lg' />
+          </a>
+          <a href="https://api.whatsapp.com/send?phone=5531992479530&text=Ol%C3%A1%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es.">
+            <IoLogoWhatsapp className='size-10  bg-gray-700/60 hover:bg-[#00B5FF]/60 p-2 rounded-lg' />
+          </a>
         </div>
 
         <div className='border-b pb-10'>
           <div>
             <h6 className='text-white text-lg font-semibold py-4'>Institucional</h6>
-            <ul className='space-y-2'>
-              <li className='hover:text-[#00B5FF]'>Sobre Nós</li>
-              <li className='hover:text-[#00B5FF]'>Nossas Lojas</li>
-              <li className='hover:text-[#00B5FF]'>Trabalhe Conosco</li>
-              <li className='hover:text-[#00B5FF]'>Blog</li>
-              <li className='hover:text-[#00B5FF]'>Imprensa</li>
+            <ul className='space-y-2 text-nowrap'>
+              <li className='hover:text-[#00B5FF] w-min'>Sobre Nós</li>
+              <li className='hover:text-[#00B5FF] w-min'>Nossas Lojas</li>
+              <li className='hover:text-[#00B5FF] w-min'>Trabalhe Conosco</li>
+              <li className='hover:text-[#00B5FF] w-min'>Blog</li>
+              <li className='hover:text-[#00B5FF] w-min'>Imprensa</li>
             </ul>
           </div>
           <div>
             <h6 className='text-white text-lg font-semibold py-4'>Atendimento</h6>
-            <ul className='space-y-2'>
-              <li className='hover:text-[#00B5FF]'>Central de Ajuda</li>
-              <li className='hover:text-[#00B5FF]'>Política de Troca</li>
-              <li className='hover:text-[#00B5FF]'>Garantia</li>
-              <li className='hover:text-[#00B5FF]'>Rastreamento</li>
-              <li className='hover:text-[#00B5FF]'>FAQ</li>
+            <ul className='space-y-2 text-nowrap'>
+              <li className='hover:text-[#00B5FF] w-min'>Central de Ajuda</li>
+              <li className='hover:text-[#00B5FF] w-min'>Política de Troca</li>
+              <li className='hover:text-[#00B5FF] w-min'>Garantia</li>
+              <li className='hover:text-[#00B5FF] w-min'>Rastreamento</li>
+              <li className='hover:text-[#00B5FF] w-min'>FAQ</li>
             </ul>
           </div>
         </div>
